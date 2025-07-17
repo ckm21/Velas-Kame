a4801d185106dd29801c25902400bda2136d8464
ckm21 <jopie21k@gmail.com>
2025-07-17 14:44:39 -0600

    Update and rename Sistema-se-ales.py to app.py

--- Sistema-se-ales.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Prueba mínima", layout="centered")
-
-st.title("✅ App de prueba cargada con éxito")
-st.write("Si ves este mensaje, tu Streamlit está funcionando.")
---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("🔥 Prueba de Velas Kame")
+st.write("La app está funcionando correctamente.")
